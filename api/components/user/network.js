3c49a52e5c7c82b71930972ec5532eec77ddd18d
Bryan <bryan_desarrollo_ec@outlook.com>
2021-05-16 17:29:20 -0500

    aislando la base de datos

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const response = require('../../../network/response')
-
+const Controller = require('./controller')
 
 router.get('/', function (req, res) {
-    response.success(req, res, 'Todo correcto', 200);
+    const list = Controller.list();
+    response.success(req, res, list, 200);
 })
 
 module.exports = router;
